0ce1f526d7b3e3c99cdbf30fa14393129abeefe3
tomdb03 <tomasdallabona@gmail.com>
2022-02-09 13:51:05 -0300

    un acento

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import FooterStyles from '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={FooterStyles.FooterContainer}>
-      <p className={FooterStyles.FooterText}>Tomás Dalla Bona. Reglamento de la podrida <a href="http://www.acanomas.com/Reglamentos-Juegos-de-Naipes/1226/Podrida.htm" className={FooterStyles.FooterLink}>ac&aacute;</a>.</p>
+      <p className={FooterStyles.FooterText}>Tom&aacute;s Dalla Bona. Reglamento de la podrida <a href="http://www.acanomas.com/Reglamentos-Juegos-de-Naipes/1226/Podrida.htm" className={FooterStyles.FooterLink}>ac&aacute;</a>.</p>
     </footer>
   );
 };
